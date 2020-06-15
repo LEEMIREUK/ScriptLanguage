0fae6e687a978b1a99f35a3ae29b2eb34a6338b8
joon-so(desktop) <joonso16@gmail.com>
2020-06-15 22:29:33 +0900

    api 불러오기

--- Script_project/test.py
+++ Script_project/test.py
@@ -1,24 +1,57 @@
-from tkinter import *
-import matplotlib
-import matplotlib.pyplot as plt
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
-from matplotlib.figure import Figure
-window_width = 1000
-window_height = 600
-root = Tk()
-global_fig = None
-def create_draw(x_dots, y_dots):
-    global global_fig
-    if global_fig is not None:
-        global_fig.clf()
-    global_fig = Figure(figsize=(5, 4), dpi=100)
-    global_fig.add_subplot(111).plot(x_dots, y_dots)
-    canvas = FigureCanvasTkAgg(global_fig, master=root)  # A tk.DrawingArea.
-    canvas.draw()
-    canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
-    canvas.get_tk_widget().pack(side=TOP, fill=BOTH, expand=1)
-    root.update()
-    global_fig.canvas.draw()\
+from urllib.request import urlopen
+from urllib.parse import urlencode,unquote,quote_plus
+import urllib
+import json
+from datetime import datetime
 
-create_draw([3,4], [2,3])
-root.mainloop()
+now = datetime.now()
+key = 'MtLAG5t2b11STi2IYFynXQZdFRhAIW96u7RqSiFIB77ruJBarCvBhjuk7AmpF8w9pzxN2oLCAOaMx%2FaMyDJqmg%3D%3D'
+start_date = str(int(now.strftime('%Y%m%d'))-7)
+end_date = now.strftime('%Y%m%d')
+url = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19InfStateJson?serviceKey='+key+'&pageNo=1&numOfRows=10&startCreateDt='+start_date+'&endCreateDt='+end_date+'&'
+
+# queryParams = '?' + urlencode({ quote_plus('servicekey') : 'YourServiceKey',
+#     quote_plus('LAYERS') : 'frejaywalking',
+#     quote_plus('FORMAT') : 'image/png',
+#     quote_plus('TRANSPARENT') : 'true',
+#     quote_plus('SERVICE') : 'WMS',
+#     quote_plus('VERSION') : '1.1.1',
+#     quote_plus('REQUEST') : 'GetMap',
+#     quote_plus('SRS') : 'EPSG:900913',
+#     quote_plus('BBOX') : '14142684.718103,4505504.1936344,14147576.687913,4510396.1634438',
+#     quote_plus('width') : '2024',
+#     quote_plus('height') : '1838',
+#     quote_plus('srs') : 'EPSG:900913',
+#     quote_plus('searchYearCd') : '2015052',
+#     quote_plus('siDo') : '11',
+#     quote_plus('guGun') : '320' })
+
+request = urllib.request.Request(url)
+jsonData=json.loads(request)
+
+print ('Your Request:\n'+url)
+request.get_method = lambda: 'GET'
+response_body = urlopen(request).read()
+print ("\nResult:")
+print (response_body)
+print ("\nDataType of Result Data:")
+print (type(response_body))
+
+while True:
+    if (jsonData['response']['header']['resultMsg'] == 'OK'):
+        nTotal = jsonData['response']['body']['totalCount']
+
+        if nTotal == 0:
+            break
+
+        # for item in jsonData['response']['body']['items']['item']:
+        #     getTourPointData(item, yyyymm, jsonResult)
+        #
+        # nPage = math.ceil(nTotal / 100)
+        # if (nPagenum == nPage):
+        #     break
+        #
+        # nPagenum += 1
+
+    else:
+        break
