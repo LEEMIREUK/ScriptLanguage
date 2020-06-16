fc9930b0230e9cdb4f6b22ef7466658fc592116d
joon-so(desktop) <joonso16@gmail.com>
2020-06-17 03:53:59 +0900

    국내 시도별 코로나현황 추가

--- Script_project/korea_corona.py
+++ Script_project/korea_corona.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from matplotlib import pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
 import numpy as np
 import requests
 from bs4 import BeautifulSoup
@@ -24,14 +25,10 @@ def corona(window):
     data = soup_korea.find_all('decidecnt')
     for code in data:
         decideCnt_korea.append(code.text)
-    lb2 = tk.Label(window, text="국내 코로나 발생 현황", bg='aquamarine', font=20)
-    lb2.place(x=220, y=40)
-    fig = plt.Figure(figsize=(5, 4), dpi=100)
+    fig = plt.Figure(figsize=(5, 2.4), dpi=100)
     ax = fig.add_subplot(1,1,1)
     fig.patch.set_facecolor('aquamarine')
     ax.set_ylim([0, 100])
-    ax.set_xlabel('날짜', size=10)
-    ax.set_ylabel('확진자수', size=10)
     x=np.array([round((int((now + timedelta(days=-6)).strftime('%Y%m%d')) - 20200000)/100, 2),
                 round((int((now + timedelta(days=-5)).strftime('%Y%m%d')) - 20200000)/100, 2),
                 round((int((now + timedelta(days=-4)).strftime('%Y%m%d')) - 20200000)/100, 2),
@@ -46,4 +43,35 @@ def corona(window):
                 int(decideCnt_korea[5]) - int(decideCnt_korea[6])])
     ax.plot(x,y,marker='o')
     canvas = FigureCanvasTkAgg(fig, window)
-    canvas._tkcanvas.place(x=50, y=65)
+    canvas._tkcanvas.place(x=100, y=10)
+    lb2 = tk.Label(window, text="국내 코로나 발생 현황", bg='aquamarine', font=8)
+    lb2.place(x=270, y=15)
+
+    #도시별 현황
+    key_world = 'MtLAG5t2b11STi2IYFynXQZdFRhAIW96u7RqSiFIB77ruJBarCvBhjuk7AmpF8w9pzxN2oLCAOaMx%2FaMyDJqmg%3D%3D'
+    url_world = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19SidoInfStateJson?serviceKey='+key_world+'&pageNo=1&numOfRows=10&startCreateDt='+ (now + timedelta(days=-1)).strftime('%Y%m%d') +'&endCreateDt='+(now + timedelta(days=-1)).strftime('%Y%m%d')+'&'
+    request_world = requests.get(url_world)
+    soup_world = BeautifulSoup(request_world.content, 'html.parser')
+    decideCnt_world = []
+    #데이터 넣기
+    data_world = soup_world.find_all('item')
+    for code in data_world:
+        decide = code.find('defcnt')
+        country = code.find('gubun')
+        decideCnt_world.append((int(decide.text), country.text))
+    #확진자 많은 순서대로 정렬
+    decideCnt_world.sort(key=lambda element : -element[0])
+    ff = Figure(figsize=(5,2.4), dpi=100)
+    xx = ff.add_subplot(111)
+    ff.patch.set_facecolor('aquamarine')
+    xx.set_ylim([10, 7000])
+    ind = [decideCnt_world[1][1], decideCnt_world[2][1], decideCnt_world[3][1], decideCnt_world[4][1], decideCnt_world[5][1], decideCnt_world[6][1],
+           decideCnt_world[7][1], decideCnt_world[8][1], decideCnt_world[9][1], decideCnt_world[10][1], decideCnt_world[11][1], decideCnt_world[12][1]]
+    datalst = [decideCnt_world[1][0], decideCnt_world[2][0], decideCnt_world[3][0], decideCnt_world[4][0], decideCnt_world[5][0], decideCnt_world[6][0],
+               decideCnt_world[7][0], decideCnt_world[8][0], decideCnt_world[9][0], decideCnt_world[10][0], decideCnt_world[11][0], decideCnt_world[12][0]]
+    rects1 = xx.bar(ind, datalst, 0.7)
+    canvas = FigureCanvasTkAgg(ff, master=window)
+    canvas.get_tk_widget().pack(side=tk.RIGHT)
+    canvas._tkcanvas.place(x=100, y=255)
+    lb2 = tk.Label(window, text="국내 시,도별 코로나 발생 현황", bg='aquamarine', font=8)
+    lb2.place(x=230, y=255)
--- Script_project/world_corona.py
+++ Script_project/world_corona.py
@@ -17,8 +17,6 @@ def corona(window):
     #현황 그래프 생성
     now = datetime.now()
     #막대그래프
-    lb2 = tk.Label(window, text="해외 코로나 발생 현황(단위 : 백만)", bg='aquamarine', font=20)
-    lb2.place(x=180, y=40)
     key_world = 'MtLAG5t2b11STi2IYFynXQZdFRhAIW96u7RqSiFIB77ruJBarCvBhjuk7AmpF8w9pzxN2oLCAOaMx%2FaMyDJqmg%3D%3D'
     url_world = 'http://openapi.data.go.kr/openapi/service/rest/Covid19/getCovid19NatInfStateJson?serviceKey='+key_world+'&pageNo=1&numOfRows=10&startCreateDt='+ (now + timedelta(days=-1)).strftime('%Y%m%d') +'&endCreateDt='+(now + timedelta(days=-1)).strftime('%Y%m%d')+'&'
     request_world = requests.get(url_world)
@@ -32,7 +30,7 @@ def corona(window):
         decideCnt_world.append((int(decide.text), country.text))
     #확진자 많은 순서대로 정렬
     decideCnt_world.sort(key=lambda element : -element[0])
-    ff = Figure(figsize=(5,4), dpi=100)
+    ff = Figure(figsize=(5,5), dpi=100)
     xx = ff.add_subplot(111)
     ff.patch.set_facecolor('aquamarine')
     xx.set_ylim([50000, 2500000])
@@ -43,6 +41,8 @@ def corona(window):
     rects1 = xx.bar(ind, datalst, 0.7)
     canvas = FigureCanvasTkAgg(ff, master=window)
     canvas.get_tk_widget().pack(side=tk.RIGHT)
-    canvas._tkcanvas.place(x=50, y=65)
+    canvas._tkcanvas.place(x=100, y=10)
+    lb2 = tk.Label(window, text="해외 코로나 발생 현황(단위 : 백만)", bg='aquamarine', font=20)
+    lb2.place(x=230, y=20)
 
 #window.mainloop()
