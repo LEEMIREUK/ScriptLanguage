2bdcbef26357db8bb0c831f3d9a62d3447983896
LEEMIREUK <32861131+LEEMIREUK@users.noreply.github.com>
2020-06-17 00:52:57 +0900

    ui 수정 및 기능 추가

--- Script_project/home.py
+++ Script_project/home.py
@@ -3,14 +3,21 @@ from tkinter import font
 from tkinter import colorchooser
 from PIL import Image, ImageTk
 
+#한글 적용
+from matplotlib import font_manager, rc
+font_name = font_manager.FontProperties(fname="c:/Windows/Fonts/malgun.ttf").get_name()
+rc('font', family=font_name)
+
 import webbrowser
 import world_data
 import hospital_map
 
+
+
 window = Tk()
-window.geometry("1000x600+500+200")          #창 크기
-window.configure(background='snow')          #배경색
-window.title("Corona")                       #창 제목
+window.geometry("800x500+500+200")          #창 크기
+window.configure(background='aquamarine')   #배경색
+window.title("Corona")                      #창 제목
 
 news ='https://search.naver.com/search.naver?query=%EC%BD%94%EB%A1%9C%EB%82%98&where=news&ie=utf8&sm=nws_hty'
 
@@ -20,58 +27,64 @@ news ='https://search.naver.com/search.naver?query=%EC%BD%94%EB%A1%9C%EB%82%98&w
 
 
 # 국가별 확진자 현황 버튼
-def listbutton1():
-    #TempFont = font.Font(window, size=20, weiht = 'bold', family ='Consolas')
-#    lb1 = Button(window, text="문구 정하기1", width=25, height=5, command=lambda: world_data.Print_worldMap(window))
-    lb1 = Button(window, text="문구 정하기1", width=25, height=5, command=lambda: onclick('<Button-1>'))
-    lb1.place(x=800, y=120)
-    lb1['bg'] = '#cffed1'
-    lb1['fg'] = '#2924ee'
+def WorldButton():
+    world_button = Button(window)
+    file_image = Image.open('image/지구.png')
+    file_image = file_image.resize((60, 60), Image.ANTIALIAS)
+    world_button.image = ImageTk.PhotoImage(file_image)
+    world_button = Button(image = world_button.image, comman=lambda:())
+    world_button.place(x=710, y=10)
 
 # 국내 확진자 현황 버튼
-def listbutton2():
-    lb2 = Button(window, text="문구 정하기2", width=25, height=5)
-    lb2.place(x=800, y=240)
-    lb2['bg'] = '#cffed1'
+def KoreaButton():
+    korea_button = Button(window)
+    file_image = Image.open('image/한국.png')
+    file_image = file_image.resize((60, 60), Image.ANTIALIAS)
+    korea_button.image = ImageTk.PhotoImage(file_image)
+    korea_button = Button(image = korea_button.image, comman=lambda:())
+    korea_button.place(x=710, y=90)
 
 # 국내 공적 마스크 판매 위치 및 제고 버든
-def listbutton3():
-    lb3 = Button(window, text="문구 정하기3", width=25, height=5)
-    lb3.place(x=800, y=360)
-    lb3['bg'] = '#cffed1'
+def MaskButton():
+    mask_button = Button(window)
+    file_image = Image.open('image/마스크.png')
+    file_image = file_image.resize((60, 60), Image.ANTIALIAS)
+    mask_button.image = ImageTk.PhotoImage(file_image)
+    mask_button = Button(image = mask_button.image, comman=lambda:())
+    mask_button.place(x=710, y=170)
 
 #국내 코로나 검진 병원 정보 버튼
 def HospitalButton():
-    hospital_Button = Button(window)
+    hospital_button = Button(window)
     file_image = Image.open('image/병원.png')
-    file_image = file_image.resize((80, 80), Image.ANTIALIAS)
-    hospital_Button.image = ImageTk.PhotoImage(file_image)
-    hospital_Button = Button(image = hospital_Button.image, comman=lambda: Open_Map())
-    hospital_Button.place(x=700, y=10)
+    file_image = file_image.resize((60, 60), Image.ANTIALIAS)
+    hospital_button.image = ImageTk.PhotoImage(file_image)
+    hospital_button = Button(image = hospital_button.image, comman=lambda: Open_Map())
+    hospital_button.place(x=710, y=250)
 
 
 #뉴스 버튼 생성
 def NewsButton():
-    news_Button =Button(window)
+    news_button =Button(window)
     file_image = Image.open('image/뉴스.png')
-    file_image = file_image.resize((80, 80), Image.ANTIALIAS)
-    news_Button.image = ImageTk.PhotoImage(file_image)
-    news_Button = Button(image = news_Button.image, command=lambda: Open_URL())
-    news_Button.place(x=800,y=10)
+    file_image = file_image.resize((60, 60), Image.ANTIALIAS)
+    news_button.image = ImageTk.PhotoImage(file_image)
+    news_button = Button(image = news_button.image, command=lambda: Open_URL())
+    news_button.place(x=710,y=330)
 
 #챗봇 버튼 생성
 def TelebotButton():
-    telbot_Button = Button(window)
+    telbot_button = Button(window)
     file_image = Image.open('image/텔레그램봇.jpg')
-    file_image = file_image.resize((80, 80), Image.ANTIALIAS)
-    telbot_Button.image = ImageTk.PhotoImage(file_image)
-    telbot_Button = Button(image = telbot_Button.image)
-    telbot_Button.place(x=900,y=10)
+    file_image = file_image.resize((60, 60), Image.ANTIALIAS)
+    telbot_button.image = ImageTk.PhotoImage(file_image)
+    telbot_button = Button(image = telbot_button.image)
+    telbot_button.place(x=710,y=410)
 
 def ShowUI():
-    listbutton1()
-    listbutton2()
-    listbutton3()
+    WorldButton()
+    KoreaButton()
+    MaskButton()
     HospitalButton()
     NewsButton()
     TelebotButton()
@@ -87,8 +100,7 @@ def Open_URL():
 
 # 병원 지도 열기
 def Open_Map():
-    hi=[12]
-
+    hospital_map.Hospital_List(window)
 
 ShowUI()
 window.mainloop()
--- Script_project/hospital_map.py
+++ Script_project/hospital_map.py
@@ -1,29 +1,107 @@
-# import requests
-# import folium
-# import webbrowser
-#
-#
-# response = requests.get(URL+Key+page)
-# status = response.status_code
-# text = response.text
-#
-# latitude = []
-# longitude = []
-# coordinate = []
-# # 위도 경도 지정
-# map_osm = folium.Map (location = [37.568477, 126.981611], zoom_start=17)
-# # 마커 지정
-# folium.Marker([37.568477, 126.981611], popup='Mt. Hood Meadows').add_to(map_osm)
-# # html 파일로 저장
-# #map_osm.save('osm.html')
-# webbrowser.open('osm.html') # web browser에 저장된 지도 띄우기
-
-import xml.etree.ElementTree as ET
 import requests
+from bs4 import BeautifulSoup
+import webbrowser
+from tkinter import *
+import tkinter.ttk
+
 URL = 'http://apis.data.go.kr/B551182/pubReliefHospService/getpubReliefHospList?serviceKey='
 Key ='MtLAG5t2b11STi2IYFynXQZdFRhAIW96u7RqSiFIB77ruJBarCvBhjuk7AmpF8w9pzxN2oLCAOaMx%2FaMyDJqmg%3D%3D'
 page ='&pageNo=1&numOfRows=1079&spclAdmTyCd=&'
+table_name =["NO.", "주소", "이름", "전화번호", "정보"]
 
 res = requests.get(URL+Key+page)
-status = res.status_code
-text = res.text
+soup = BeautifulSoup(res.content, 'html.parser')
+data = soup.find_all('item')
+
+def Hospital_List(window):
+    data_list = []
+    for code in data:
+        do = code.find('sidonm')
+        city = code.find('sggunm')
+        name = code.find('yadmnm')
+        tel = code.find('telno')
+        kind = code.find('spcladmtycd')
+        if kind =='A0':
+            kind = "국민안심병원"
+        elif kind == '97':
+            kind = "코로나검사 실시기관"
+        elif kind == '99':
+            kind = "선별진료소 운영기관"
+        data_list.append((do.text + city.text, name.text, tel.text, kind.text))
+
+    print(data_list)
+
+    fput = tkinter.Frame(window)
+    fput.pack()
+    lb1 = tkinter.Label(fput, text=" 주소 ")
+    lb1.place(x=200, y=100)
+    lb1.configure(foreground='greenyellow')
+    iptAddr = tkinter.Entry(fput, width=20)
+    iptAddr.insert(0, data_list)
+    iptAddr.place(x=240, y=280)
+    search = tkinter.Button(fput, text="검색", width=10, command=lambda : ())
+    search.place(x=390, y=280)
+
+    treeview = tkinter.ttk.Treeview(window, columns=["one", "two", "three", "four", "five"],
+                                    displaycolum=["one", "two", "three", "four", "five"])
+    treeview.column("#0", width=50, anchor="center")
+    treeview.heading("#0", text=table_name[0])
+    treeview.column("#1", width=100, anchor="center")
+    treeview.heading("#1", text=table_name[1])
+    treeview.column("#2", width=100, anchor="center")
+    treeview.heading("#2", text=table_name[2]  )
+    treeview.column("#3", width=100, anchor="center")
+    treeview.heading("#3", text=table_name[3])
+    treeview.column("#4", width=100, anchor="center")
+    treeview.heading("#4", text=table_name[4])
+    for i in range(len(data_list)):
+        treeview.insert('' , 'end', text=i, values=data_list[i], iid=str(i)+"번")
+    treeview.place(x=10, y=150)
+
+
+# def Searchinfo():
+#     fput
+#     # 기존 데이터 있다면 삭제
+#     for row in trv.get_children():
+#         trv.delete(row)
+#
+#     global stores
+#     jData = requetJson()
+#     # 데이터를 제대로 받아오지 못했다면 메세지창으로 알림
+#     if type(jData) == int:
+#         tkMsg.showerror("실패", "요청을 실패하였습니다.")
+#         return -1
+#     # print(type(jData)) # dict
+#     # print info(stores)
+#     cnt = 0
+#     for store in jData['stores']:
+#         # remain_stat : None, empty, few, some, plenty
+#         # empty : 회색(0~1개)/
+#         # few: 빨강색(2~29개)/some: 노랑색(30~99개)/ plenty: 녹색(100개 이상)
+#         status = store.get('remain_stat')
+#         if status in ["few", "some", "plenty"]:
+#             # header = ["판매처","주소","재고량","입고일시","생성일시"]
+#             info = (store.get('name'),
+#                     store.get('addr').replace(iptAddr.get(), "")
+#                     , status, store.get('stock_at')
+#                     , store.get('created_at'))
+#             stores.append(info)
+#             cnt += 1
+#             # 재고 상태에 따라 색상 변경 해줄려고 재고 상태를 태그로 달아줌
+#             trv.insert('', 'end', text=cnt, values=info, tags=[status])
+#             # print(stores) #추출한 데이터 확인
+#     # 재고 상태별 행 색상 변경(적용이 안된다면 3.3 버그 해결부분 참고)
+#     trv.tag_configure('plenty', background="green")
+#     trv.tag_configure('some', background="yellow")
+#     trv.tag_configure('few', background="red")
+
+
+
+
+
+
+
+#https://map.naver.com/v5/search/도%20시%20기관명/place
+#URL = 'https://map.naver.com/v5/search/'+do[0] +'%20'+city[0]+'%20'+tel[0]+'/place'
+#print(data_list)
+#webbrowser.open(URL)
