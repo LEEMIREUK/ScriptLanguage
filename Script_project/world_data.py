c86af8a9bd6b29136154d90b498462685c63fb12
LEEMIREUK <32861131+LEEMIREUK@users.noreply.github.com>
2020-06-16 12:57:38 +0900

    ui수정 추가

--- Script_project/world_data.py
+++ Script_project/world_data.py
@@ -1,7 +1,11 @@
-import numpy
-import matplotlib.pyplot as plt
 from tkinter import *
 from PIL import Image, ImageTk
+from matplotlib import pyplot as plt
+from matplotlib import animation, rc
+
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
+import numpy as np
 import time
 
 def Print_worldMap(window):
@@ -11,17 +15,25 @@ def Print_worldMap(window):
     worldmap = Label(image = worldmap.image)
     worldmap.place(x=100, y=150)
 
-def Show_PieGraph(window):
+def Pie_Graph(window):
+    # lb1 = Label(window, text='대륙별 코로나 확진자 비율')
+    # lb1.place(x=200, y=100)
     g_continent = ['Africa', 'Europe', 'America', 'Middle East', 'Asia', 'Oceania']
     g_value = [10, 20, 30, 10, 25, 5]
-    g_color = ['#aa5555', '#27b920', '#ca88ab', '#a2a2a2', '#f1ff9f', '#78c4fc']
-    plt.pie(g_value, labels=g_continent, colors=g_color, shadow=True, startangle=90)
+    g_color = ('coral', 'yellowgreen', 'lightcoral', 'thistle', 'orange', 'lightskyblue')
+    pie = plt.pie(g_value, labels=g_continent, colors=g_color, autopct='%1.1f%%',
+    shadow=True, startangle=90, textprops={'fontsize': 9})
+    plt.axis('equal')
+
+    plt.pie(g_value, labels=g_continent, colors=g_color, autopct='%1.1f%%',
+            shadow=True, startangle=90, textprops={'fontsize': 9})
+    plt.axis('equal')
+    plt.title('대륙별 코로나 확진자 비율', fontsize=14)
     plt.show(window)
 
 
-def Show_Graph(window):
-    x_country = ()
-    y_value =()
-    index = numpy.arange()
+def Bar_Graph(window):
+    fig, ax = plt.subplot()
+
 
 #    plt.bar(index, y1_value, bar_width,
