020ac3109523e29ce4dcef306ae58f809e38a7eb
LEEMIREUK <32861131+LEEMIREUK@users.noreply.github.com>
2020-06-17 04:51:12 +0900

    병원 지도 구현

--- Script_project/hospital_map.py
+++ Script_project/hospital_map.py
@@ -7,14 +7,14 @@ import tkinter.ttk
 URL = 'http://apis.data.go.kr/B551182/pubReliefHospService/getpubReliefHospList?serviceKey='
 Key ='MtLAG5t2b11STi2IYFynXQZdFRhAIW96u7RqSiFIB77ruJBarCvBhjuk7AmpF8w9pzxN2oLCAOaMx%2FaMyDJqmg%3D%3D'
 page ='&pageNo=1&numOfRows=1079&spclAdmTyCd=&'
-table_name =["NO.", "주소", "이름", "전화번호", "정보"]
+table_name =["NO.", "시·도", "구·군", "이름", "전화번호", "정보"]
 
 res = requests.get(URL+Key+page)
 soup = BeautifulSoup(res.content, 'html.parser')
 data = soup.find_all('item')
 
 data_list = []
-global treeview
+
 
 def Hospital_List(window):
     global data_list
@@ -26,16 +26,11 @@ def Hospital_List(window):
         kind = str((code.find('spcladmtycd')).string.strip())
         if kind == 'A0':
             kind = '국민안심병원'
-        if kind == '97':
-            kind = '코로나검사 실시기관'
-        kind = code.find('spcladmtycd')
-        if kind == 'A0':
-            kind = "국민안심병원"
         elif kind == '97':
             kind = "코로나검사 실시기관"
         elif kind == '99':
             kind = '선별진료소 운영기관'
-        data_list.append((do.text + city.text, name.text, tel.text, kind))
+        data_list.append((do.text, city.text, name.text, tel.text, kind))
     print(data_list)
 
     # 검색
@@ -43,57 +38,57 @@ def Hospital_List(window):
     lb1 = tkinter.Label(window, width=10, text=" 주소 ")
     lb1.configure(background='aquamarine')
     lb1.place(x=50, y=103)
+
+    global iptAddr
     iptAddr = tkinter.Entry(window, width=40)
     iptAddr.insert(0, default_search)
     iptAddr.place(x=130, y=103)
-    search = tkinter.Button(window, text="검색", width=10, command=lambda :Info_List(treeview))
+    search = tkinter.Button(window, text="검색", width=10, command=lambda: Info_List())
     search.place(x=430, y=100)
 
     # 목록
-    treeview = tkinter.ttk.Treeview(window, height=15, columns=["one", "two", "three", "four"],
-                                    displaycolum=["one", "two", "three", "four"],
+    global treeview
+    treeview = tkinter.ttk.Treeview(window, height=15, columns=["one", "two", "three", "four", "five"],
+                                    displaycolum=["one", "two", "three", "four", "five"],
                                     padding=0)
     scrY = tkinter.Scrollbar(orient='vertical', command=treeview.yview)
-    scrY.place(x=675, y=151, height=250)
+    scrY.place(x=675, y=151, height=325)
     treeview.place(x=10, y=150)
     treeview.columnconfigure(0, weight=1)
-    lb1 = tkinter.Label(window, text=" 주소 ")
-    lb1.place(x=200, y=100)
-    lb1.configure(foreground='greenyellow')
-    iptAddr = tkinter.Entry(window, width=20)
-    iptAddr.insert(0, data_list)
-    iptAddr.place(x=240, y=280)
-    search = tkinter.Button(window, text="검색", width=10, command=lambda: ())
-    search.place(x=390, y=280)
 
     treeview.column("#0", width=50, anchor="center")
     treeview.heading("#0", text=table_name[0])
-    treeview.column("#1", width=100, anchor="center")
+    treeview.column("#1", width=50, anchor="center")
     treeview.heading("#1", text=table_name[1])
-    treeview.column("#2", width=230, anchor="center")
+    treeview.column("#2", width=50, anchor="center")
     treeview.heading("#2", text=table_name[2])
-    treeview.column("#3", width=150, anchor="center")
+    treeview.column("#3", width=230, anchor="center")
     treeview.heading("#3", text=table_name[3])
     treeview.column("#4", width=150, anchor="center")
     treeview.heading("#4", text=table_name[4])
+    treeview.column("#5", width=150, anchor="center")
+    treeview.heading("#5", text=table_name[5])
     for i in range(len(data_list)):
         treeview.insert('', 'end', text=i+1, values=data_list[i])
-        treeview.insert('', 'end', text=i, values=data_list[i], iid=str(i)+"번")
-    treeview.place(x=10, y=150)
 
-    treeview.tag_bind()
+    treeview.bind("<Double-1>", DoubleClick)
 
 def DoubleClick(event):
-    selectlist = treeview.item(treeview.selectiono()[0])
-
+    get_do = treeview.item(treeview.selection()[0])['values'][0]
+    get_si = treeview.item(treeview.selection()[0])['values'][1]
+    get_name = treeview.item(treeview.selection()[0])['values'][2]
+    print(get_do)
+    print(get_si)
+    print(get_name)
     #https://map.naver.com/v5/search/도%20시%20기관명/place
-    #URL = 'https://map.naver.com/v5/search/'+[0] +'%20'+city[0]+'%20'+tel[0]+'/place'
-    print(data_list)
+    URL = 'https://map.naver.com/v5/search/'+get_do+'%20'+get_si+'%20'+get_name+'/place'
     webbrowser.open(URL)
 
-def Info_List(event):
+def Info_List():
     for row in treeview.get_children():
         treeview.delete(row)
+
+    for store in data_list['stores']:
 
     print(data_list)
 
