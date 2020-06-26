18e7343e500a4a864636f7e38658054f556ecceb
joon-so(desktop) <joonso16@gmail.com>
2020-06-26 14:42:14 +0900

    화면전환 완료, mask코드정리

--- Script_project/home.py
+++ Script_project/home.py
@@ -1,6 +1,4 @@
 from tkinter import *
-from tkinter import font
-from tkinter import colorchooser
 from PIL import Image, ImageTk
 
 #한글 적용
@@ -31,45 +29,45 @@ news ='https://search.naver.com/search.naver?query=%EC%BD%94%EB%A1%9C%EB%82%98&w
 
 
 # 국가별 확진자 현황 버튼
-def WorldButton():
-    world_button = Button(window)
+def WorldButton(Frame):
+    world_button = Button(Frame)
     file_image = Image.open('image/지구.png')
     file_image = file_image.resize((60, 60), Image.ANTIALIAS)
     world_button.image = ImageTk.PhotoImage(file_image)
-    world_button = Button(image = world_button.image, comman=lambda:Open_world_corona())
+    world_button = Button(image = world_button.image, comman=lambda:Open_world_corona(Frame))
     world_button.place(x=710, y=10)
 
 # 국내 확진자 현황 버튼
-def KoreaButton():
-    korea_button = Button(window)
+def KoreaButton(Frame):
+    korea_button = Button(Frame)
     file_image = Image.open('image/한국.png')
     file_image = file_image.resize((60, 60), Image.ANTIALIAS)
     korea_button.image = ImageTk.PhotoImage(file_image)
-    korea_button = Button(image = korea_button.image, comman=lambda: Open_korea_corona())
+    korea_button = Button(image = korea_button.image, comman=lambda: Open_korea_corona(Frame))
     korea_button.place(x=710, y=90)
 
 # 국내 공적 마스크 판매 위치 및 제고 버든
-def MaskButton():
-    mask_button = Button(window)
+def MaskButton(Frame):
+    mask_button = Button(Frame)
     file_image = Image.open('image/마스크.png')
     file_image = file_image.resize((60, 60), Image.ANTIALIAS)
     mask_button.image = ImageTk.PhotoImage(file_image)
-    mask_button = Button(image = mask_button.image, comman=lambda: Open_Mask())
+    mask_button = Button(image = mask_button.image, comman=lambda: Open_Mask(Frame))
     mask_button.place(x=710, y=170)
 
 #국내 코로나 검진 병원 정보 버튼
-def HospitalButton():
-    hospital_button = Button(window)
+def HospitalButton(Frame):
+    hospital_button = Button(Frame)
     file_image = Image.open('image/병원.png')
     file_image = file_image.resize((60, 60), Image.ANTIALIAS)
     hospital_button.image = ImageTk.PhotoImage(file_image)
-    hospital_button = Button(image = hospital_button.image, comman=lambda: Open_Map())
+    hospital_button = Button(image = hospital_button.image, comman=lambda: Open_Map(Frame))
     hospital_button.place(x=710, y=250)
 
 
 #뉴스 버튼 생성
-def NewsButton():
-    news_button =Button(window)
+def NewsButton(Frame):
+    news_button =Button(Frame)
     file_image = Image.open('image/뉴스.png')
     file_image = file_image.resize((60, 60), Image.ANTIALIAS)
     news_button.image = ImageTk.PhotoImage(file_image)
@@ -77,33 +75,47 @@ def NewsButton():
     news_button.place(x=710,y=330)
 
 #챗봇 버튼 생성
-def TelebotButton():
-    telbot_button = Button(window)
+def TelebotButton(Frame):
+    telbot_button = Button(Frame)
     file_image = Image.open('image/텔레그램봇.jpg')
     file_image = file_image.resize((60, 60), Image.ANTIALIAS)
     telbot_button.image = ImageTk.PhotoImage(file_image)
     telbot_button = Button(image = telbot_button.image)
     telbot_button.place(x=710,y=410)
 
-def ShowUI():
-    WorldButton()
-    KoreaButton()
-    MaskButton()
-    HospitalButton()
-    NewsButton()
-    TelebotButton()
+def ShowUI(Frame):
+    WorldButton(Frame)
+    KoreaButton(Frame)
+    MaskButton(Frame)
+    HospitalButton(Frame)
+    NewsButton(Frame)
+    TelebotButton(Frame)
 
 def onclick(event, list):
-    world_data.Print_worldMap(window)
+    world_data.Print_worldMap(Frame)
 #    world_data.Show_Graph(window)
-    world_data.Pie_Graph(window)
+    world_data.Pie_Graph(Frame)
 
-def Open_korea_corona():
-    korea_corona.corona(window)
+def Open_korea_corona(Frame):
+    Frame.destroy()
+    new_Frame = Tk()
+    new_Frame.geometry("800x500+500+200")  # 창 크기
+    new_Frame.configure(background='aquamarine')  # 배경색
+    new_Frame.title("Corona")  # 창 제목
+    new_Frame.resizable(False, False)  # 화면크기조절 불가능
+    ShowUI(new_Frame)
+    korea_corona.corona(new_Frame)
 
 
-def Open_world_corona():
-    world_corona.corona(window)
+def Open_world_corona(Frame):
+    Frame.destroy()
+    new_Frame = Tk()
+    new_Frame.geometry("800x500+500+200")  # 창 크기
+    new_Frame.configure(background='aquamarine')  # 배경색
+    new_Frame.title("Corona")  # 창 제목
+    new_Frame.resizable(False, False)  # 화면크기조절 불가능
+    ShowUI(new_Frame)
+    world_corona.corona(new_Frame)
 
 
 # 네이버 뉴스창 열어주기
@@ -112,13 +124,26 @@ def Open_URL():
 
 
 # 병원 지도 열기
-def Open_Map():
-    hospital_map.Hospital_List(window)
+def Open_Map(Frame):
+    Frame.destroy()
+    new_Frame = Tk()
+    new_Frame.geometry("800x500+500+200")  # 창 크기
+    new_Frame.configure(background='aquamarine')  # 배경색
+    new_Frame.title("Corona")  # 창 제목
+    new_Frame.resizable(False, False)  # 화면크기조절 불가능
+    ShowUI(new_Frame)
+    hospital_map.Hospital_List(new_Frame)
 
 
-def Open_Mask():
-    mask.mask(window)
+def Open_Mask(Frame):
+    Frame.destroy()
+    new_Frame = Tk()
+    new_Frame.geometry("800x500+500+200")  # 창 크기
+    new_Frame.configure(background='aquamarine')  # 배경색
+    new_Frame.title("Corona")  # 창 제목
+    new_Frame.resizable(False, False)  # 화면크기조절 불가능
+    ShowUI(new_Frame)
+    mask.mask(new_Frame)
 
-
-ShowUI()
+ShowUI(window)
 window.mainloop()
--- Script_project/mask.py
+++ Script_project/mask.py
@@ -3,14 +3,6 @@ import json
 import tkinter as tk
 import tkinter.ttk as ttk
 import webbrowser as wbs
-import tkinter.messagebox as tkMsg
-from matplotlib import pyplot as plt
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import numpy as np
-import requests
-from bs4 import BeautifulSoup
-from datetime import datetime, timedelta
 from matplotlib import font_manager, rc
 font_name = font_manager.FontProperties(fname="c:/Windows/Fonts/malgun.ttf").get_name()
 rc('font', family=font_name, size=8.3)
@@ -19,7 +11,7 @@ rc('font', family=font_name, size=8.3)
 def mask(window):
     mask_url = "https://8oi9s0nnth.apigw.ntruss.com/corona19-masks/v1/storesByAddr/json"
 
-    addr = "경기도 시흥시" #default address
+    addr = "경기도 시흥시"
     header = ["판매처","주소","재고량","입고일시","생성일시"]
     global stores
     stores = [] # store list
@@ -27,30 +19,14 @@ def mask(window):
     #해당 주소로 판매처 및 재고현황 정보 요청
     def requetJson():
         global addr
-        # 응답코드(status_code) 반환
-        # 200: 성공, 404:  존재하지 않는 URL
         addr = iptAddr.get()
         res = rq.get(mask_url, params={"address": addr})  # rq.post()
-        print(res.status_code)
-        if res.status_code == 200:
-            print("[요청성공]")
-        else:
-            print("[알수 없는 에러:%s]\n " % res)
-            return -1
 
         # load json data
         return json.loads(res.content)
 
 
     def fixed_map(option):
-        # Fix for setting text colour for Tkinter 8.6.9
-        # From: https://core.tcl.tk/tk/info/509cafafae
-        #
-        # Returns the style map for 'option' with any styles starting with
-        # ('!disabled', '!selected', ...) filtered out.
-
-        # style.map() returns an empty list for missing options, so this
-        # should be future-safe.
         return [elm for elm in style.map('Treeview', query_opt=option) if
           elm[:2] != ('!disabled', '!selected')]
 
@@ -63,30 +39,17 @@ def mask(window):
 
         global stores
         jData = requetJson()
-        # 데이터를 제대로 받아오지 못했다면 메세지창으로 알림
-        if type(jData) == int:
-            tkMsg.showerror("실패", "요청을 실패하였습니다.")
-            return -1
-        # print(type(jData)) # dict
-        # print info(stores)
         cnt = 0
         for store in jData['stores']:
-            # remain_stat : None, empty, few, some, plenty
-            # empty : 회색(0~1개)/
-            # few: 빨강색(2~29개)/some: 노랑색(30~99개)/ plenty: 녹색(100개 이상)
             status = store.get('remain_stat')
             if status in ["few", "some", "plenty"]:
-                # header = ["판매처","주소","재고량","입고일시","생성일시"]
                 info = (store.get('name'),
                         store.get('addr').replace(iptAddr.get(), "")
                         , status, store.get('stock_at')
                         , store.get('created_at'))
                 stores.append(info)
                 cnt += 1
-                # 재고 상태에 따라 색상 변경 해줄려고 재고 상태를 태그로 달아줌
                 trv.insert('', 'end', text=cnt, values=info, tags=[status])
-                #print(stores) #추출한 데이터 확인
-        # 재고 상태별 행 색상 변경(적용이 안된다면 3.3 버그 해결부분 참고)
         trv.tag_configure('plenty', background="green")
         trv.tag_configure('some', background="yellow")
         trv.tag_configure('few', background="red")
@@ -96,20 +59,11 @@ def mask(window):
      더블클릭시 해당 판매처 위치 검색
     '''
     def OnDoubleClick(e):
-    #treeview에서 선택된 아이템정보 가져오기
       selectedItem = trv.item(trv.selection()[0])['values']
-      # 0: name, 1: addr, 2: remain (주소와 판매처 이름 합치기)
       selectedAddr = iptAddr.get()+selectedItem[1]
-      # print(selectedAddr) #합친 주소정보 확인
       googleURL = "https://www.google.com/search?ei=FLDfXv6hI4qRr7wPmbGH8A8&q="
       param = selectedAddr+" "+selectedItem[0]
-      print(param) # 보낼 데이터 확인
       wbs.open(googleURL+param, new=1) #해당 주소로 인터넷창 팝업
-
-
-    # window = tk.Tk()
-    # window.geometry("900x550")
-    # window.title("공적 마스크 판매 현황") #창 제목
 
 
     # 입력받는 곳
@@ -118,7 +72,7 @@ def mask(window):
     lb1 = tk.Label(window, text = "마스크 검색",bg='aquamarine')
     lb1.configure(foreground='blue')
     lb1.place(x=135, y=120)
-    iptAddr = tk.Entry(window,width=20) #주소 입력받는 인풋 위젯
+    iptAddr = tk.Entry(window,width=20)
     iptAddr.insert(0, addr)
     iptAddr.place(x=250, y=120)
     btnSearch = tk.Button(window, text="Search", width=10, command=infoSearch)
@@ -139,9 +93,6 @@ def mask(window):
     lb2.place(x=20, y=450)
 
 
-    # 판매현황표시 --> treeview(like table)
-
-    #fInput.pack(fill=tk.BOTH, expand=1)
     trv = ttk.Treeview(window, columns=header, displaycolumns=header, padding=0)
 
 
@@ -173,4 +124,3 @@ def mask(window):
       background=fixed_map('background'))
 
     trv.bind("<Double-1>", OnDoubleClick)
-#window.mainloop()
